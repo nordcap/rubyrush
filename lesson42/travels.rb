79ebffef8fa3edf253eeb4d65adfd2bcf9149525
Alexey Budaev <budaevaa@mail.ru>
2021-01-26 21:29:05 +0700

    lesson42 - travels

---
+++ lesson42/travels.rb
@@ -0,0 +1,10 @@
+require_relative './paris'
+require_relative './rome'
+require_relative './london'
+
+puts "Начали путешествие"
+visit_paris
+visit_rome
+visit_london
+puts "Закончили путешествие"
+
